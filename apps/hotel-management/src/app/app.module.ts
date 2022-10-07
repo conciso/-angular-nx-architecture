db3a7c18b1f12deb48efb8d7cc5382fcf92e24ef
KevinClever <kevin.clever@conciso.de>
2022-10-07 16:34:41 +0200

    install angular material

--- apps/hotel-management/src/app/app.module.ts
+++ apps/hotel-management/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, LandingPageComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
