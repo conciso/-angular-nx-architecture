624df5473426109a6af3b416fd667f12c8f0ed29
KevinClever <kevin.clever@conciso.de>
2022-10-07 16:28:14 +0200

    add landing page

--- apps/hotel-management/src/app/app.module.ts
+++ apps/hotel-management/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, LandingPageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
