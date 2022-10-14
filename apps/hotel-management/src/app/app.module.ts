d03a4431972cd2395b53a67e4bbdeb9853271c75
KevinClever <kevin.clever@conciso.de>
2022-10-14 16:34:04 +0200

    add ddd template for luggage

--- apps/hotel-management/src/app/app.module.ts
+++ apps/hotel-management/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {AppComponent} from './app.component';
-import {NxWelcomeComponent} from './nx-welcome.component';
-import {LandingPageComponent} from './landing-page/landing-page.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RoomSelectionPageComponent} from './rooms/room-selection-page/room-selection-page.component';
-import {RoomViewPageComponent} from './rooms/room-view-page/room-view-page.component';
-import {RoomComponent} from './rooms/room-selection-page/room/room.component';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomSelectionPageComponent } from './rooms/room-selection-page/room-selection-page.component';
+import { RoomViewPageComponent } from './rooms/room-view-page/room-view-page.component';
+import { RoomComponent } from './rooms/room-selection-page/room/room.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LuggageFeatureCheckinModule } from '@hotel-management/luggage/feature-checkin';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import {HttpClientModule} from '@angular/common/http';
     MatIconModule,
     MatButtonModule,
     MatCardModule,
+    LuggageFeatureCheckinModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ libs/luggage/domain/src/index.ts
@@ -0,0 +1,5 @@
+null;
+export * from './lib/entities/luggage';
+export * from './lib/infrastructure/luggage.data.service';
+
+export * from './lib/application/checkin.facade';
